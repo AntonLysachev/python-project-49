8a02c6819a0bed3edca80262ee417d924cb8d6cb
AntonLysachev <anton@lysachev.ru>
2023-08-13 11:27:25 +0500

    modified cli.py and brain_games.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -2,14 +2,16 @@
 import sys
 sys.path.append('d:\\Github\\python-project-49\\brain_games')
 from cli import welcome_user
+
+
 def greetings():
     print('Welcome to the Brain Games!')
     welcome_user()
 
+
 def main():
     greetings()
-    
-    
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     main()
