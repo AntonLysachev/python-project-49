a53fe47f242cc8db21f8f97f99255633d844e74d
AntonLysachev <anton@lysachev.ru>
2023-08-13 09:55:46 +0500

    Add function "welcome_user". ver. 0.2.0

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
-
+import sys
+sys.path.append('d:\\Github\\python-project-49\\brain_games')
+from cli import welcome_user
 def greetings():
     print('Welcome to the Brain Games!')
+    welcome_user()
 
 def main():
     greetings()
+    
+    
 
 if __name__=='__main__':
     main()
