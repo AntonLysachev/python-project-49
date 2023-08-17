0211db6a23d493205410febeb1abc8910cb1568f
AntonLysachev <anton@lysachev.ru>
2023-08-17 12:53:37 +0500

    Fix calc.py

--- brain_games/calculation/calc.py
+++ brain_games/calculation/calc.py
@@ -4,11 +4,16 @@ import random
 def game():
     num1 = random.randint(1, 20)
     num2 = random.randint(1, 20)
-    if (random.randint(1, 10)) % 2:
-        result = num1 * num2
-        meaning = f'{num1} * {num2}'
-    else:
-        result = num1 + num2
-        meaning = f'{num1} + {num2}'
+    choice = random.randint(1,9) % 3
+    match choice:
+        case 0:
+            result = num1 * num2
+            meaning = f'{num1} * {num2}'
+        case 1:
+            result = num1 - num2
+            meaning = f'{num1} - {num2}'
+        case 2:
+            result = num1 + num2
+            meaning = f'{num1} + {num2}'
     data = (meaning, str(result))
     return data
