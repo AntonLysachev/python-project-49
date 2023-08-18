ea7f42f198ee00f8d11dd7c35577a37523bed2cb
AntonLysachev <anton@lysachev.ru>
2023-08-18 17:32:06 +0500

    updata rpime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -7,7 +7,7 @@ DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 def simple_or_not(numb):
     if numb == 1:
         return False
-    for i in range(numb - 2):
+    for i in range(2, int(numb ** 0.5 + 1)):
         if not (numb % (i + 2)):
             return False
     return True
