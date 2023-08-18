e56e668993b16ff783883628c29325280dce8c74
AntonLysachev <anton@lysachev.ru>
2023-08-18 17:28:48 +0500

    uodata calc.py

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -6,15 +6,15 @@ DESCRIPTION = 'What is the result of the expression?'
 def generate():
     num1 = random.randint(1, 20)
     num2 = random.randint(1, 20)
-    choice = random.randint(1, 9) % 3
-    match choice:
-        case 0:
+    operator = random.choice('+-*')
+    match operator:
+        case '*':
             right = num1 * num2
             question = f'{num1} * {num2}'
-        case 1:
+        case '-':
             right = num1 - num2
             question = f'{num1} - {num2}'
-        case 2:
+        case '+':
             right = num1 + num2
             question = f'{num1} + {num2}'
     data = (question, str(right))
