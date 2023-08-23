f40f8f3958a7083f549a98dd6fec763fb37c563b
AntonLysachev <anton@lysachev.ru>
2023-08-23 11:58:00 +0500

    fix operator

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -5,7 +5,7 @@ DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def is_prime(number):
-    if number < 1:
+    if number < 2:
         return False
     for i in range(2, int(number ** 0.5 + 1)):
         if not (number % i):
