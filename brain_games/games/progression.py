434de3d9f4dcf78447c12fb4aafcd686480a1746
AntonLysachev <anton@lysachev.ru>
2023-08-23 11:58:51 +0500

    add inside constant

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -1,28 +1,31 @@
 import random
-from brain_games import constant
 
 
 DESCRIPTION = 'What number is missing in the progression?'
+SIZE_PROGRESSION = 10
 
 
 def generate_progression():
-    progression = []
     start = random.randint(1, 100)
     rule = random.randint(2, 5)
-    for i in range(constant.SIZE):
-        if i == 0:
-            progression.append(i + start)
-        else:
-            progression.append(progression[i - 1] + rule)
+    progression = [start]
+    for i in range(SIZE_PROGRESSION):
+        progression.append(progression[i] + rule)
     return progression
 
 
 def generate_round_data():
     progression = generate_progression()
-    random_index = random.randint(0, constant.SIZE - 1)
+    random_index = random.randint(0, SIZE_PROGRESSION - 1)
     right = progression[random_index]
     progression[random_index] = '..'
     question = ''
-    for i in progression:               # не понял как тут использовать map
-        question += str().join(f'{i} ')  # question = map(str.join, progression)
-    return question, str(right)         # на выходе адрес обекта
+    for i in progression:
+        question += str().join(f'{i} ')
+    return question, str(right)
+# не понял как тут использовать map
+# question = map(str.join, progression)
+# на выходе адрес обекта
+# при конвертации list(map(str.join, progression))
+# ошибка
+# TypeError: descriptor 'join' for 'str' objects doesn't apply to a 'int' object
